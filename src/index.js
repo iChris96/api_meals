b1c5e97cdca036eda226f54fc00cb864b9f86c80
LHI1GA <christopheralejandro.lariosperez@mx.bosch.com>
2021-03-11 11:22:59 -0600

    defining routes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 3000;
+const mealsRoutes = require('./routes/MealsRoutes')
 
-
+// Api config
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+mongoose.connect('mongodb://localhost:27017/meals', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
+// Routes config
+app.use('/meals', mealsRoutes);
 
 app.get('*', (req, res) => {
     res.send("welcome to meals api");
 })
+
+
+// Listening config
 
 app.listen(port, () => {
     console.log(`Meals app listening at http://localhost:${port}`)
